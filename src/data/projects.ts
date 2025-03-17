05e6ffc39711b49f17e4bc636d850174be090216
felipealvx <felipealvk@gmail.com>
2025-03-16 22:38:25 -0300

    adicionando imagens dos projetos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,28 +1,48 @@
-import Img1 from "../assets/imgs/icon.jpg"
+import Img1 from "../assets/imgs/projects/petPlanner.png";
+import Img2 from "../assets/imgs/projects/qrCodeGen.png";
+import Img3 from "../assets/imgs/projects/pokeNative.png";
+import Img4 from "../assets/imgs/projects/drinkNative.png";
+import Img5 from "../assets/imgs/projects/IJornadaEdf.png";
 
 export const projects = [
   {
     id: 1,
-    title: "PokeNative",
-    description: "Projeto para utilizar a API de Pokémon, PokeAPI, fazendo um projeto prático usando React Native.",
+    title: "PetPlanner",
+    description: "O PetPlanner é um aplicativo desenvolvido para auxiliar tutores de pets no gerenciamento da saúde e bem-estar de seus bichinhos. Com ele, é possível registrar o período de vacinação dos pets e definir lembretes para auxiliar no cuidado",
     img: Img1,
-    link: "https://github.com/",
-    stacks: ["React Native", "Expo", "TypeScript"]
+    link: "https://github.com/felipealvx/petsAppSwift",
+    stacks: ["SwiftUI", "Figma"]
   },
   {
     id: 2,
     title: "QR-CodeGen",
-    description: "Gerador de QR code utilizando React Native com a biblioteca React-Native-QRCode-Svg ",
-    img: Img1,
-    link: "https://github.com/",
+    description: "Gerador de QR code utilizando React Native com a biblioteca React-Native-QRCode-Svg, o app possibilita a criação de QR codes personalizados de textos ou links.",
+    img: Img2,
+    link: "https://github.com/felipealvx/qrCodeGen",
     stacks: ["React Native", "Javascript", "Expo"]
   },
   {
     id: 3,
-    title: "Pets Native",
-    description: "App para cadastrar seus pets e adicionar lembretes das vacinas e gerenciamento/cuidado do seu pet.",
-    img: Img1,
-    link: "https://github.com/",
-    stacks: ["SwiftUI", "React Native", "Typescript"]
+    title: "PokeNative",
+    description: "Pokedex usando ReactNative e a API PokeAPI para listar os pokemons e suas informações, com a possibilidade de pesquisar e ver detalhes de cada pokemon.",
+    img: Img3,
+    link: "https://github.com/felipealvx/nativePokedex",
+    stacks: ["React Native", "Typescript"]
+  },
+  {
+    id: 4,
+    title: "DrinkNative",
+    description: "Pesquise drinks e veja suas receitas, com a possibilidade de pesquisar por nome ou categoria, o app foi desenvolvido com React Native e a API TheCocktailDB.",
+    img: Img4,
+    link: "https://github.com/felipealvx/drinks-app-mobile",
+    stacks: ["React Native", "Typescript"]
+  },
+  {
+    id: 5,
+    title: "I Jornada de Educação Física: Esporte e Saúde",
+    description: "Site para o evento I Jornada de Educação Física: Esporte e Saúde da Universidade Regional do Cariri, desenvolvido com HTML, CSS e Javascript permite facilitar o acesso a informações do Evento.",
+    img: Img5,
+    link: "https://github.com/jornadaedf",
+    stacks: ["HTML", "CSS", "Javascript"]
   }
 ]
